6b3d167acb379d7493340f19881132a5b7346157
k-washi <kai.washizaki@gmail.com>
2019-10-18 04:18:11 +0900

    modify lint error

--- src/domain/collectinfo/collectinfo_items.go
+++ src/domain/collectinfo/collectinfo_items.go
@@ -6,26 +6,31 @@ package collectinfo
 	  POST: req: {"name": "name", "description": "test", "data": {"id": 1, "name": "test1" }}, res: {"name": "name", "message": "ok"}
 */
 
+//CreateCollectInfoRequest request of post method
 type CreateCollectInfoRequest struct {
 	Name        string      `json:"name"`
 	Description string      `json:"description"`
 	Data        CollectInfo `json:"data"`
 }
 
+//CreateCollectInfoResponse response of post method
 type CreateCollectInfoResponse struct {
 	Name    string `json:"name"`
 	Message string `json:"message"`
 }
 
+//CollectInfo request data
 type CollectInfo struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
 
+//GetCollectInfoRequest request of get method
 type GetCollectInfoRequest struct {
 	Name string `json:"name"`
 }
 
+//GetCollectInfoResponse response of get method
 type GetCollectInfoResponse struct {
 	Name        string        `json:"name"`
 	Description string        `json:"description"`
